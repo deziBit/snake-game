2eae2816a2295af95fb8b3bdbc980a8c82a5d5d5
deziBit <d_becher@gmx.de>
2019-12-22 19:40:24 +0100

    Add draw function

--- Game.js
+++ Game.js
@@ -5,12 +5,17 @@ class Game{
         this.cellSize = cellSize;
         this.isRunning = false;
         this.snake = new Snake({x: 10, y: 1});
+        this.snakeColor = '#101010';
     }
     start(){
         this.isRunning = true;
         this.run();
     }
     run(){
-
+        this.draw(this.snake.head, this.snakeColor);
+    }
+    draw(position, color){
+        this.ctx.fillStyle = color;
+		this.ctx.fillRect(position.x * this.cellSize, position.y * this.cellSize, this.cellSize, this.cellSize);
     }
 }
