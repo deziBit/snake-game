9ae135140b6e541914ca9c97e4d7c0db36e118b3
deziBit <d_becher@gmx.de>
2019-12-24 16:06:59 +0100

    Update changeDirection function

--- Snake.js
+++ Snake.js
@@ -10,16 +10,20 @@ class Snake{
     changeDirection(newDirection){
         switch(newDirection){
 			case 'left':
-                this.direction = {val: 'left', vec: {x: -1, y: 0}};
+                if(this.direction.val != 'right')
+                    this.direction = {val: 'left', vec: {x: -1, y: 0}};
 				break;
 			case 'up':
-                this.direction = {val: 'up', vec: {x: 0, y: -1}};
+                if(this.direction.val != 'down')
+                    this.direction = {val: 'up', vec: {x: 0, y: -1}};
 				break;
 			case 'right':
-                this.direction = {val: 'right', vec: {x: 1, y: 0}};
+                if(this.direction.val != 'left')
+                    this.direction = {val: 'right', vec: {x: 1, y: 0}};
 				break;
 			case 'down':
-                this.direction = {val: 'down', vec: {x: 0, y: 1}};
+                if(this.direction.val != 'up')
+                    this.direction = {val: 'down', vec: {x: 0, y: 1}};
 				break;
 			default:
 				break;
